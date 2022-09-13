86d1ffa6c73dd723bc6e2a6d11945bb433db76ce
PietroPasotti <pietro.pasotti@canonical.com>
2022-09-13 10:33:39 +0200

    Initial commit

---
+++ scripts/update.sh
@@ -0,0 +1,7 @@
+# abort on errors
+set -e
+
+tox || echo "TESTS FAILED! aborting..." && exit 1
+PYTHONPATH=$PYTHONPATH:./ ./scripts/bump-version.py
+PYTHONPATH=$PYTHONPATH:./ ./scripts/inline-lib.py
+./scripts/publish.sh
